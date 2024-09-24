1d91bfeb2b32e1eb6907c40f9dc99935802a95bc
HumpBackJack <147660746+HumpBackJack@users.noreply.github.com>
2024-09-24 10:02:06 -0400

    Update FirstFile.py

--- FirstFile.py
+++ FirstFile.py
@@ -1,7 +1,7 @@
 # Jack Gordon 9/23/24
-first = "Jack"
+first = "Jack" + "  "
 last = "Gordon"
 age = 15
-print(first + last)
-print(first, last)
-print(first, last, str(age))
+print(first + " " + last)
+print(first,last, )
+print(first,last, str(age))
